5e3ab03dd9750303ad9b9159c594c021d23981f9
swayminin <swayminin@yandex.ru>
2023-11-19 20:27:16 +0500

    Initial release

--- doc_management_project/settings.py
+++ doc_management_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d-gl65d^(ho*mac+=kyq#n2m3jgs-i0^!4=a)x-g+nh*-sqjr1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
